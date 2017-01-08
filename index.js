17f2f83894b21f15089d6d8178024661afcd3c3e
carson hyde <carsonmhyde@gmail.com>
2017-01-08 18:59:45 -0500

    fixes merge conflict

---
+++ index.js
@@ -0,0 +1,24 @@
+const remote = require('electron').remote
+const main = remote.require('./main.js')
+
+var mikoButton = document.createElement('button')
+mikoButton.textContent = 'Miko\'s Game'
+mikoButton.addEventListener('click', () => {
+  main.openMiko()
+}, false)
+document.body.appendChild(mikoButton)
+
+
+var connieButton = document.createElement('button')
+connieButton.textContent = 'Connie\'s Game'
+connieButton.addEventListener('click', () => {
+  main.openConnie()
+}, false)
+document.body.appendChild(connieButton)
+
+var carsonButton = document.createElement('button')
+carsonButton.textContent = 'Carson\'s Game'
+carsonButton.addEventListener('click', () => {
+  main.openCarson()
+}, false)
+document.body.appendChild(carsonButton)
---
+++ main.js
@@ -0,0 +1,90 @@
+// 'use strict';
+// const electron = require('electron');
+// const remote = require('electron').remote;
+// const app = electron.app;
+
+// // adds debug features like hotkeys for triggering dev tools and reload
+// require('electron-debug')();
+
+// // prevent window being garbage collected
+// let mainWindow;
+
+// function onClosed() {
+// 	// dereference the window
+// 	// for multiple windows store them in an array
+// 	mainWindow = null;
+// }
+
+// function createMainWindow() {
+// 	const win = new electron.BrowserWindow({
+// 		width: 800,
+// 		height: 600,
+// 		frame: false,
+// 		resizable: false
+// 	});
+
+// 	win.loadURL(`file://${__dirname}/app/index.html`);
+// 	win.on('closed', onClosed);
+
+// 	return win;
+// }
+
+// app.on('window-all-closed', () => {
+// 	if (process.platform !== 'darwin') {
+// 		app.quit();
+// 	}
+// });
+
+// 	function minimizeWin(){
+//     	var window = remote.getCurrentWindow();
+//     window.minimize();
+//   }
+
+//  	function closeWin(){
+//     var window = remote.getCurrentWindow();
+//     window.close();
+//   }
+
+// app.on('activate', () => {
+// 	if (!mainWindow) {
+// 		mainWindow = createMainWindow();
+// 	}
+// });
+
+// app.on('ready', () => {
+// 	mainWindow = createMainWindow();
+// });
+
+// require the electron API
+const electron = require('electron')
+// electron is an object so we can pull off a few things off that object, like so:
+// const app = electron.app
+// const BrowserWindow = electron.BrowserWindow
+// or you can use ES6 destructuring
+const {app, BrowserWindow} = require('electron')
+
+// call this function, on ready
+app.on('ready', () => {
+  // this will make a new browser window for us, with a width of 800 and height of 600, you can change these values if you'd like
+  const mainWindow = new BrowserWindow({ width: 800, height:600})
+  // this will load a window for us, you can put any url in here like 'http://google.com' or you can use a file path
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  // file://${__dirname} checks in our local directory, remember to use backticks ` for this
+  mainWindow.openDevTools();
+
+})
+
+exports.openMiko= () => {
+  let miko = new BrowserWindow({width: 412, height: 732})
+  miko.loadURL(`file://${__dirname}/miko/index.html`)
+}
+
+exports.openConnie = () => {
+  let connie = new BrowserWindow({width: 412, height: 732})
+  connie.loadURL(`file://${__dirname}/connie/index.html`)
+}
+
+exports.openCarson= () => {
+  let carson = new BrowserWindow({width: 412, height: 732})
+  carson.loadURL(`file://${__dirname}/carson/index.html`)
+}
